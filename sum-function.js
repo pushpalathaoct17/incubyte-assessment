3cb876d610972f0e9cc8f4f80f98e6544a50a22e
pentirajasekhar <pushpalathaoct17@gmail.com>
2024-10-19 20:18:20 +0530

    Handling new lines and commas between numbers

--- sum-function.js
+++ sum-function.js
@@ -2,7 +2,7 @@ function add(numbers) {
   if (!numbers || numbers === "") {
     return 0;
   } else {
-    let digits = numbers.split(",");
+    let digits = numbers.split(/[\n,]/);
     let sum = 0;
     for (let i = 0; i < digits.length; i++) {
       sum = sum + parseInt(digits[i]);
@@ -16,3 +16,12 @@ console.log(" empty input :", add("")); // output - 0
 console.log(" single digit input :", add("1")); // output - 1
 
 console.log(" mulitple digits with comma separated input :", add("1,5")); // output - 6
+
+console.log(" mulitple digits with comma separated input :", add("1,5, 1,6")); // output - 13
+
+console.log(" Handling new lines between numbers :", add("1\n2,3")); // output - 6
+
+console.log(
+  " Handling new lines between numbers with multple :",
+  add("1\n2,3\n4,5")
+); // output - 15
