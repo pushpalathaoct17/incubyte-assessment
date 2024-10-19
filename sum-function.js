394a7f2d717a255038d49a041ed05cc048f9afe8
pentirajasekhar <pushpalathaoct17@gmail.com>
2024-10-19 20:28:47 +0530

    Handling Custom Delimiters

--- sum-function.js
+++ sum-function.js
@@ -2,7 +2,13 @@ function add(numbers) {
   if (!numbers || numbers === "") {
     return 0;
   } else {
-    let digits = numbers.split(/[\n,]/);
+    let delimiter = /[\n,]/;
+    if (numbers.startsWith("//")) {
+      const parts = numbers.split("\n");
+      delimiter = new RegExp(parts[0][2]);
+      numbers = parts[1];
+    }
+    let digits = numbers.split(delimiter);
     let sum = 0;
     for (let i = 0; i < digits.length; i++) {
       sum = sum + parseInt(digits[i]);
@@ -25,3 +31,5 @@ console.log(
   " Handling new lines between numbers with multple :",
   add("1\n2,3\n4,5")
 ); // output - 15
+
+console.log(" Handling different delimiters: :", add("//;\n1;2")); // output - 3
